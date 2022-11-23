6084c025785370af78da7fee46c099e404c5e22d
santoshnirajan <santosh.shahi475@gmail.com>
2022-11-24 02:33:49 +0545

    Post Fueltype

--- PetrolKhata/Model/FuelType.cs
+++ PetrolKhata/Model/FuelType.cs
@@ -5,5 +5,9 @@
         public int Id { get; set; }
         public string Fuel { get; set; }
         public ICollection<Sales>Sales { get; set; }
+         public FuelType ()
+        {
+            Sales = new List<Sales> (); 
+        }
     }
 }
